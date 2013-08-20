d31179600649965b19e7ffcabb53213bef404908
David Pedersen <david.pdrsn@gmail.com>
2013-08-20 14:20:13 +0200

    build

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'jasmine'
+gem 'closure-compiler'
+gem 'coffee-script'
---
+++ build.rb
@@ -0,0 +1,23 @@
+require 'closure-compiler'
+require 'coffee-script'
+
+compiler = Closure::Compiler.new(compilation_level: 'ADVANCED_OPTIMIZATIONS')
+
+files = [
+  'src/lib/split.js',
+  'src/parser.coffee'
+]
+
+js = files.inject '' do |result, js_component|
+  result += if js_component =~ /.*\.coffee$/
+              CoffeeScript.compile File.read(js_component)
+            else
+              File.read(js_component)
+            end
+end
+
+contents = compiler.compile js
+
+File.open 'build/parser.js', "w" do |file|
+  file.write contents
+end
